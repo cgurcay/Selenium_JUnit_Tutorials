e7b874cdf402b9500086de1412f64d1a2497da4f
cgurcay <cgurcay7@gmail.com>
2025-07-01 10:31:53 +0300

    beforeeach and aftereach

---
+++ src/test/java/tests/Junit_Framework/junit_beforeEach_afterEach.java
@@ -0,0 +1,65 @@
+package tests.Junit_Framework;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class junit_beforeEach_afterEach {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void youtubeTest() {
+        driver.get("https://youtube.com");
+
+        String expectedTitle = "YouTube";
+        String actualTitle = driver.getTitle();
+
+        System.out.print("Youtube Title Test: ");
+        if (expectedTitle.equals(actualTitle)) {
+            System.out.println("PASSED");
+        } else System.out.println("FAILED");
+    }
+
+    @Test
+    public void wiseQuarterTest() {
+        driver.get("https://wisequarter.com");
+
+        String expectedTitle = "https://wisequarter.com/";
+        String actualTitle = driver.getCurrentUrl();
+
+        System.out.print("Wise Quarter URL Test: ");
+        if (expectedTitle.equals(actualTitle)) {
+            System.out.println("PASSED");
+        } else System.out.println("FAILED");
+    }
+
+    @Test
+    public void arabamTest() {
+        driver.get("https://arabam.com");
+
+        String expectedTitle = "arabam.com";
+        String actualTitle = driver.getTitle();
+
+        System.out.print("Arabam Title Test: ");
+        if (actualTitle.contains(expectedTitle)) {
+            System.out.println("PASSED");
+        } else System.out.println("FAILED");
+    }
+}
---
+++ src/test/java/tests/Junit_Framework/junit_testMethodOrderer.java
@@ -0,0 +1,35 @@
+package tests.Junit_Framework;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+// @TestMethodOrder(MethodOrderer.MethodName.class) // A-Z by name (MethodName or DisplayName)
+public class junit_testMethodOrderer {
+
+    /*
+        Without - MethodOrderer.OrderAnnotation.class
+        Third Test
+        First Test
+        Second Test
+     */
+
+    @Test @Order(1)
+    public void zfirstTest() {
+        System.out.println("First Test");
+    }
+
+    @Test @Order(2)
+    public void secondTest() {
+        System.out.println("Second Test");
+    }
+
+    @Test @Order(3)
+    public void thirdTest() {
+        System.out.println("Third Test");
+    }
+
+
+}
